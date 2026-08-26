5d436a23bae852d8cbae2aa3e578cdff828c9fe6
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-08-26 09:57:21 +0300

    chore: bump version to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sl.passwordgenerator"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.4.4"
+        versionCode = 11
+        versionName = "1.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
