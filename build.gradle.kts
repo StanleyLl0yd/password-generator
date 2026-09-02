e5abf2701a65049d6fc37418538325c21db49099
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-02 08:55:09 +0200

    build: resolve verified JavaPoet on plugin classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
+buildscript {
+    dependencies {
+        classpath("com.squareup:javapoet:1.13.0")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.detekt) apply false
-}
-
-subprojects {
-    configurations.configureEach {
-        resolutionStrategy.force("com.squareup:javapoet:1.13.0")
-    }
 }
