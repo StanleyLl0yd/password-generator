57a423af96d4128ba92ed86559269f33239b7e6b
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-18 15:54:31 +0300

    android: align source version with 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,8 +106,8 @@ android {
         applicationId = "com.sl.passwordgenerator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.5.6"
+        versionCode = 18
+        versionName = "1.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
