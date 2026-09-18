463333bd4e16fd6a33f8d7d0a24e301ac737b755
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-18 15:51:14 +0300

    windows: add native Win32 application

---
+++ windows/src/ui.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "clipboard.hpp"
+#include "generator.hpp"
+
+#include <windows.h>
+
+#include <string>
+
+namespace pg {
+
+class MainWindow {
+public:
+    bool Create(HINSTANCE instance, int showCommand);
+    int Run();
+
+private:
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+    static HRESULT CALLBACK AboutCallback(
+        HWND hwnd,
+        UINT notification,
+        WPARAM wParam,
+        LPARAM lParam,
+        LONG_PTR referenceData
+    );
+
+    LRESULT HandleMessage(UINT message, WPARAM wParam, LPARAM lParam);
+    void CreateControls();
+    void LayoutControls(int width, int height);
+    void LoadState();
+    void SaveState();
+    void Generate();
+    void CopyPassword();
+    void SetRevealed(bool revealed);
+    void UpdateLength(int length);
+    void UpdatePreferencesFromControls();
+    void UpdateStrength(int score);
+    void ShowAbout();
+    void ShowStatus(const std::wstring& text);
+    void ClearPassword();
+
+    static bool IsRussian();
+    static std::wstring Widen(const std::string& value);
+
+    HINSTANCE instance_ = nullptr;
+    HWND hwnd_ = nullptr;
+    HACCEL accelerators_ = nullptr;
+
+    HWND passwordEdit_ = nullptr;
+    HWND revealButton_ = nullptr;
+    HWND copyButton_ = nullptr;
+    HWND strengthBar_ = nullptr;
+    HWND strengthLabel_ = nullptr;
+    HWND lengthLabel_ = nullptr;
+    HWND lengthSlider_ = nullptr;
+    HWND lowerCheck_ = nullptr;
+    HWND upperCheck_ = nullptr;
+    HWND digitCheck_ = nullptr;
+    HWND symbolCheck_ = nullptr;
+    HWND similarCheck_ = nullptr;
+    HWND duplicateCheck_ = nullptr;
+    HWND generateButton_ = nullptr;
+    HWND statusLabel_ = nullptr;
+
+    Preferences preferences_;
+    PasswordGenerator generator_;
+    SecureClipboard clipboard_;
+    std::string password_;
+    bool revealed_ = false;
+    bool russian_ = false;
+};
+
+}  // namespace pg
